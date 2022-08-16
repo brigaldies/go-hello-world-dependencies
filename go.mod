d21d574051d009c135cffb8b0442e561ef91c16b
Bertrand Rigaldies <brigaldies@gmail.com>
2022-08-16 13:03:19 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-hello-world-dependencies
+
+go 1.18
+
+require github.com/brigaldies/go-hello-world v0.1.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/brigaldies/go-hello-world/morestrings"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+}
